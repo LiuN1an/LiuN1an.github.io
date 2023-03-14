589cc7d5ea01f96d91c875b9c949c6cfafd71607
hongkewei.liun1an <hongkewei.liun1an@bytedance.com>
2023-03-14 14:02:43 +0800

    feat: github markdown recomd
    Change-Id: I446726719e5434d6ef021d4a6f0b52342545e15e

--- src/pages/resume.jsx
+++ src/pages/resume.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 
 // https://emojipedia.org/party-popper/
+// https://github.com/Hubery-Lee/Notes/blob/master/%E5%A6%82%E4%BD%95%E5%9C%A8github%E4%B8%8A%E5%86%99%E5%87%BA%E6%BC%82%E4%BA%AE%E7%9A%84readme.md.md
 export const Resume = () => {
   return (
     <div className="min-h-screen bg-gray-50 py-8 flex flex-col  relative overflow-hidden lg:py-12">
