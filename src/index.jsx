9ebda8db86c12a55094fbca3c800ae228c16ab84
hongkewei.liun1an <hongkewei.liun1an@bytedance.com>
2023-03-14 13:58:59 +0800

    feat: router opt
    Change-Id: Ifa99119448cfc5377dc438d0d58ae16f7275c838

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,11 @@
 import { App } from "./app";
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import "./style.css";
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>
+  </HashRouter>
 );
